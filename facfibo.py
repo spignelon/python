90202f400e3580836351e9d59e78694fcab7b8e4
spignelon <92091338+spignelon@users.noreply.github.com>
2022-07-15 21:58:21 +0530

    fixes and updates

--- facfibo.py
+++ facfibo.py
@@ -3,12 +3,9 @@ b = 1
 for i in range(1,a+1):
     b = i*b
 print("Factorial is:",b)
-x=0
-y=1
-b=1
-print("Fibonacci series:", x, y, end=" ")
+x, y, b = 0, 1, 0
+print("Fibonacci series:", x , y, end=" ")
 for i in range(a-1):
-    b=x+y
-    x=y
-    y=b
+    b = x + y
+    x, y = y, b
     print(b, end=" ")
