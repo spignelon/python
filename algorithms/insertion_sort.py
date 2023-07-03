d58afe6ece95998e6bb2c145386a6eea1d97b432
Ujjawal Saini <92091338+spignelon@users.noreply.github.com>
2023-07-03 18:31:49 +0530

    added insertion_sort

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > key:
+            A[i + 1] = A[i]
+            i -= 1
+        A[i + 1] = key
+
+A = [5, 2, 4, 6, 1, 3]
+insertion_sort(A)
+print(A)
