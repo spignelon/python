95a3ad59bf0a76771ec246645dea4876a2632809
Ujjawal Saini <92091338+spignelon@users.noreply.github.com>
2023-07-04 14:35:59 +0530

    Added bruteforce_sorting

---
+++ algorithms/bruteforce_sorting.py
@@ -0,0 +1,10 @@
+A = [-5, -23, 5, 0, 23, -6, 23, 67]
+C = []
+while A:
+    minimum = A[0]
+    for x in A:
+        if x < minimum:
+            minimum = x
+    C.append(minimum)
+    A.remove(minimum)
+print(C)
